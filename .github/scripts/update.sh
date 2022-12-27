296bab232f0802bd7333fe9e4ae499c6d0518e28
José Loarca <joseloarca97@icloud.com>
2022-12-27 09:42:44 -0600

    updated script

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -5,3 +5,4 @@ poetry update --dry-run
 ts=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "This file was added via GitHub Actions on $ts" > "gactions-$ts.txt"
 exec git add "gactions-$ts.txt"
+exec git commit -m "Added gactions-$ts.txt file"
